619584b3884e1439be7fec585cb69b581aa7f78c
James Kettle <albinowax@gmail.com>
2018-03-15 16:20:45 +0000

    Read URLs from file, display interesting results

--- src/fast-http.kt
+++ src/fast-http.kt
@@ -1,3 +1,4 @@
+import java.lang.Integer.max
 import java.net.InetAddress
 import java.net.URL
 import java.security.Security
@@ -10,23 +11,38 @@ import javax.net.ssl.SSLSocketFactory
 import javax.net.ssl.TrustManager
 import javax.net.ssl.X509TrustManager
 import kotlin.concurrent.thread
+import java.io.IOException
+import jdk.nashorn.internal.objects.NativeArray.forEach
+import java.io.File
+import java.io.InputStream
+import java.nio.file.Paths
+import java.nio.file.Files
+import java.util.function.Consumer
+import java.util.stream.Stream
+
+
 
 
 fun main(args : Array<String>) {
-    val url = URL(args[0])
-    val requests = args[1].toInt();
-    val threads = args[2].toInt()
-    val requestsPerConnection = args[3].toInt()
-    val readFreq = args[4].toInt();
+    val urlfile = args[0]
+    val threads = args[1].toInt()
+    val requestsPerConnection = args[2].toInt()
 
+    var readFreq = requestsPerConnection
+    if(args.size > 3) {
+        readFreq = args[3].toInt();
+    }
 
+    val inputStream: InputStream = File(urlfile).inputStream()
+    val urls = ArrayList<ArrayList<String>>()
+    for(i in 1..threads) {
+        urls.add(ArrayList<String>())
+    }
 
-    val requestsPerThread = requests / threads;
-    val request = ("GET ${url.path} HTTP/1.1\r\n"
-                  +"Host: ${url.host}\r\n"
-                  +"Connection: keep-alive\r\n"
-                  +"\r\n").toByteArray(Charsets.ISO_8859_1)
+    var i = 0
+    inputStream.bufferedReader().useLines { lines -> lines.forEach { urls.get(i++ % threads).add(it)} }
 
+    val url = URL(urls.get(0).get(0))
     val ipAddress = InetAddress.getByName(url.host)
     val port = if (url.port == -1) { url.defaultPort } else { url.port }
 
@@ -41,7 +57,7 @@ fun main(args : Array<String>) {
 
     for(j in 1..threads) {
         thread {
-            sendRequests(url, trustingSslSocketFactory, ipAddress, port, requestsPerThread, request, statusMap, totalBytes, latch, readFreq, requestsPerConnection)
+            sendRequests(url, trustingSslSocketFactory, ipAddress, port, urls.get(j-1), statusMap, totalBytes, latch, readFreq, requestsPerConnection)
         }
     }
     latch.await()
@@ -54,15 +70,27 @@ fun main(args : Array<String>) {
 
     val time = System.nanoTime() - start
     println("Time: " + "%.2f".format(time.toFloat() / 1000000000))
+    var requests = 0
+    for (e in urls) {
+        requests += e.size
+    }
+    println("RPS: %.0f".format(requests/(time.toFloat() / 1000000000)))
 }
 
 
-private fun sendRequests(url: URL, trustingSslSocketFactory: SSLSocketFactory, ipAddress: InetAddress?, port: Int, requestsPerThread: Int, request: ByteArray, statusMap: HashMap<Int, Int>, totalBytes: Int, latch: CountDownLatch, readFreq: Int, baseRequestsPerConnection: Int) {
+private fun sendRequests(url: URL, trustingSslSocketFactory: SSLSocketFactory, ipAddress: InetAddress?, port: Int, urls: ArrayList<String>, statusMap: HashMap<Int, Int>, totalBytes: Int, latch: CountDownLatch, baseReadFreq: Int, baseRequestsPerConnection: Int) {
+    var readFreq = baseReadFreq
     var totalBytes1 = totalBytes
     var threadBytes = 0
     val inflight = ArrayDeque<ByteArray>()
     val todo = ArrayDeque<ByteArray>();
-    for (i in 1..requestsPerThread) {
+
+    for (i in urls) {
+        val target = URL(i)
+        val request = ("GET ${target.path}?${target.query} HTTP/1.1\r\n"
+                +"Host: ${target.host}\r\n"
+                +"Connection: keep-alive\r\n"
+                +"\r\n").toByteArray(Charsets.ISO_8859_1)
         todo.add(request);
     }
 
@@ -76,20 +104,25 @@ private fun sendRequests(url: URL, trustingSslSocketFactory: SSLSocketFactory, i
             } else {
                 SocketFactory.getDefault().createSocket(ipAddress, port)
             }
+            socket.soTimeout = 10000
+            // todo tweak other TCP options for max performance
 
-            for (i in 1..requestsPerConnection/readFreq) {
+            var requestsSent = 0
+            while (requestsSent < requestsPerConnection && !todo.isEmpty()) {
 
                 var readCount = 0
                 for (j in 1..readFreq) {
-                    if (todo.isEmpty()) {
+                    if (todo.isEmpty() || requestsSent >= requestsPerConnection) {
                         break
                     }
 
                     val req = todo.pop();
-                    inflight.push(req)
+                    inflight.addLast(req)
                     socket.getOutputStream().write(req)
                     readCount++
+                    requestsSent++
                 }
+
                 val read = ByteArray(1024)
                 var buffer = ""
 
@@ -116,17 +149,24 @@ private fun sendRequests(url: URL, trustingSslSocketFactory: SSLSocketFactory, i
                     if (!msg.startsWith("HTTP")) {
                         throw Exception("no http")
                     }
+
                     val status = msg.split(" ")[1].toInt()
-                    inflight.removeFirst()
+                    val req = inflight.removeFirst()
+                    if (status != 404 && status != 401) {
+                        println("" + status + ": " + String(req).split("\n")[0])
+                    }
+
                     synchronized(statusMap) {
                         statusMap.put(status, statusMap.getOrDefault(status, 0) + 1)
                     }
                 }
             }
         } catch (ex: Exception) {
-            print("error, continuing\n")
+
             ex.printStackTrace()
-            requestsPerConnection = requestsPerThread / 2
+            //readFreq = max(1, readFreq / 2)
+            //requestsPerConnection = max(1, requestsPerConnection/2)
+            //println("Lost ${inflight.size} requests. Changing requestsPerConnection to $requestsPerConnection and readFreq to $readFreq")
             todo.addAll(inflight)
             inflight.clear()
         }
