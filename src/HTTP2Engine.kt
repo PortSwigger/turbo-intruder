b54c46a08be8f8a353c9d40d165135ba0d695c38
James Kettle <albinowax@gmail.com>
2018-04-03 13:56:56 +0100

    Add HTTP2 engine

---
+++ src/HTTP2Engine.kt
@@ -0,0 +1,431 @@
+/*
+ * ====================================================================
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ */
+//package org.apache.hc.core5.http.examples;
+package burp
+import java.util.ArrayDeque
+import java.util.concurrent.*
+
+import org.apache.hc.core5.concurrent.FutureCallback
+import org.apache.hc.core5.http.*
+import org.apache.hc.core5.http.impl.Http1StreamListener
+import org.apache.hc.core5.http.impl.bootstrap.AsyncRequesterBootstrap
+import org.apache.hc.core5.http.impl.bootstrap.HttpAsyncRequester
+import org.apache.hc.core5.http.io.entity.StringEntity
+import org.apache.hc.core5.http.message.BasicClassicHttpRequest
+import org.apache.hc.core5.http.message.BasicHttpRequest
+import org.apache.hc.core5.http.nio.*
+import org.apache.hc.core5.http.nio.entity.BasicAsyncEntityConsumer
+import org.apache.hc.core5.http.nio.entity.BasicAsyncEntityProducer
+import org.apache.hc.core5.http.nio.entity.StringAsyncEntityConsumer
+import org.apache.hc.core5.http.nio.entity.StringAsyncEntityProducer
+import org.apache.hc.core5.http2.config.H2Config
+import org.apache.hc.core5.http2.frame.RawFrame
+import org.apache.hc.core5.http2.impl.nio.Http2StreamListener
+import org.apache.hc.core5.http2.impl.nio.bootstrap.H2RequesterBootstrap
+import org.apache.hc.core5.io.ShutdownType
+import org.apache.hc.core5.reactor.IOReactorConfig
+import org.apache.hc.core5.util.Args
+import org.apache.hc.core5.util.Timeout
+import java.io.IOException
+import java.net.URI
+import java.net.URL
+import java.nio.ByteBuffer
+import java.nio.CharBuffer
+import java.nio.charset.Charset
+import java.nio.charset.StandardCharsets
+import java.util.concurrent.atomic.AtomicInteger
+import java.util.concurrent.atomic.AtomicReference
+
+
+object Http2MultiStreamExecutionExample {
+
+    @Throws(Exception::class)
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val engine = HTTP2Engine("https://research1.hackxor.net/", args[0].toInt(), args[1].toInt(), args[2].toInt(), ::callback)
+
+        val REQUESTCOUNT = args[3].toInt()
+        for (i in 0 until REQUESTCOUNT) {
+            //val requestString = "GET /static/cow HTTP/1.1\r\nHost: research1.hackxor.net\r\nConnection: keep-alive\r\n\r\n"
+            val requestString = "POST /static/cow HTTP/1.1\r\nHost: hackxor.net\r\nConnectionz: keep-alive\r\nContent-Length: 7\r\n\r\nfoo=bar"
+
+            engine.queue(requestString)
+            //Thread.sleep(10);
+        }
+        engine.start()
+        engine.showStats()
+    }
+
+
+    fun callback(req: String, resp: String): Boolean {
+        //println(req)
+        //println("-------")
+        //println(resp)
+        return false
+    }
+
+}
+
+class HTTP2Engine(val url: String, val threads: Int, val readFreq: Int, val requestsPerConnection: Int, val callback: ((String, String) -> Boolean)?): RequestEngine {
+
+
+    private val requestQueue = ArrayBlockingQueue<Request>(1000000)
+    var start: Long = 0
+    var successfulRequests = AtomicInteger(0)
+    var requester: HttpAsyncRequester
+    lateinit var latch: CountDownLatch
+    var parsed = URL(url)
+
+    init {
+        //requester = createPipe();
+        requester = createHTTP2()
+
+        Runtime.getRuntime().addShutdownHook(object : Thread() {
+            override fun run() {
+                //System.out.println("HTTP requester shutting down");
+                requester.shutdown(ShutdownType.GRACEFUL)
+            }
+        })
+        requester.start()
+
+    }
+
+
+    override fun start() {
+
+        //val target = HttpHost(url.host, url.port, url.protocol)
+        println(parsed.protocol)
+        val target = HttpHost(parsed.host, parsed.port, parsed.protocol)
+        start = System.nanoTime()
+        latch = CountDownLatch(threads)
+
+        Thread {
+            try {
+                println("Starting...")
+                for (i in 0 until threads) {
+                    Connection(requester, target, requestQueue, requestsPerConnection, readFreq, successfulRequests, latch, callback)
+                }
+
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
+
+        }.start()
+
+    }
+
+    override fun showStats() {
+        latch.await(15, TimeUnit.SECONDS)
+
+        val requests = successfulRequests.get().toFloat()
+        val duration = System.nanoTime().toFloat() - start
+        println("Sent " + requests + " requests in " + duration / 1000000000 + " seconds")
+        System.out.printf("RPS: %.0f\n", requests / (duration / 1000000000))
+        requester.initiateShutdown()
+    }
+
+    override fun queue(req: String) {
+        requestQueue.add(Request(req, parsed))
+    }
+
+    private fun createHTTP2(): HttpAsyncRequester {
+        // Create and start requester
+        val h2Config = H2Config.custom()
+                .setPushEnabled(false)
+                .setMaxConcurrentStreams(1000)
+                .build()
+        //h2Config = H2Config.DEFAULT;
+
+        val requester = H2RequesterBootstrap.bootstrap()
+                .setH2Config(h2Config).setMaxTotal(9000000).setDefaultMaxPerRoute(9000000)
+                .setStreamListener(object : Http2StreamListener {
+
+                    override fun onHeaderInput(connection: HttpConnection, streamId: Int, headers: List<Header>) {
+                        for (i in headers.indices) {
+//                            println(connection.toString() + " (" + streamId + ") << " + headers[i])
+                        }
+                    }
+
+                    override fun onHeaderOutput(connection: HttpConnection, streamId: Int, headers: List<Header>) {
+//                        for (i in headers.indices) {
+//                            println(connection.toString() + " (" + streamId + ") >> " + headers[i])
+//                        }
+                    }
+
+                    override fun onFrameInput(connection: HttpConnection, streamId: Int, frame: RawFrame) {
+//                        println("argh")
+                    }
+
+                    override fun onFrameOutput(connection: HttpConnection, streamId: Int, frame: RawFrame) {}
+
+                    override fun onInputFlowControl(connection: HttpConnection, streamId: Int, delta: Int, actualSize: Int) {}
+
+                    override fun onOutputFlowControl(connection: HttpConnection, streamId: Int, delta: Int, actualSize: Int) {}
+
+                })
+                .create()
+        return requester
+    }
+
+    private fun createPipe(): HttpAsyncRequester {
+        val ioReactorConfig = IOReactorConfig.custom()
+                .setSoTimeout(5, TimeUnit.SECONDS)
+                .build()
+
+        // Create and start requester
+
+        return AsyncRequesterBootstrap.bootstrap()
+                .setIOReactorConfig(ioReactorConfig).setMaxTotal(9000000).setDefaultMaxPerRoute(9000000)
+                .setStreamListener(object : Http1StreamListener {
+
+                    override fun onRequestHead(connection: HttpConnection, request: HttpRequest) {
+                        //System.out.println(connection + " " + new RequestLine(request));
+
+                    }
+
+                    override fun onResponseHead(connection: HttpConnection, response: HttpResponse) {
+                        //System.out.println(connection + " " + new StatusLine(response));
+                    }
+
+                    override fun onExchangeComplete(connection: HttpConnection, keepAlive: Boolean) {
+
+                    }
+
+                })
+                .create()
+    }
+}
+
+internal class Request(var base: String, var url: URL) {
+    lateinit var request: BasicClassicHttpRequest
+    var dataProducer: AsyncEntityProducer? = null
+
+    init  {
+        try {
+            val headers = base.split("\r\n\r\n".toRegex(), 2).toTypedArray()[0].split("\r\n".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+            val requestParts = headers[0].split(" ".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+
+            if (requestParts.size < 2) {
+                throw Exception("Bad request line")
+            }
+
+            request = BasicClassicHttpRequest(requestParts[0], requestParts[1])
+
+
+            for (i in 1 until headers.size - 1) {
+                val headerParts = headers[i].split(": ".toRegex(), 2).toTypedArray()
+
+                if (headerParts.size < 2) {
+                    throw Exception("Bad header: "+headerParts)
+                }
+
+                request.addHeader(headerParts[0], headerParts[1])
+            }
+
+            request.scheme = url.protocol
+
+            val body = base.split("\r\n\r\n".toRegex(), 2).toTypedArray()
+            if (body.size > 1 && "" != body[1]) {
+                val entity = StringEntity(body[1])
+                entity.isChunked = false
+                request.setEntity(entity)
+                //dataProducer = BasicAsyncEntityProducer(body[1], ContentType.APPLICATION_FORM_URLENCODED)
+                dataProducer = StringAsyncEntityProducer(body[1], 7, 7, ContentType.APPLICATION_FORM_URLENCODED)
+            }
+            else {
+                dataProducer = null
+            }
+
+
+        } catch (e: Exception) {
+            println("Error creating request from input string. If the request is malformed, you may need to use the non-async approach")
+            e.printStackTrace()
+        }
+    }
+
+}
+
+internal class Connection(private val requester: HttpAsyncRequester, private val target: HttpHost, private val requestQueue: ArrayBlockingQueue<Request>, private val requestsPerConnection: Int, private val readFreq: Int, private val successfulRequests: AtomicInteger, val latch: CountDownLatch, val callback: ((String, String) -> Boolean)?) : FutureCallback<Message<HttpResponse, String>> {
+    private var clientEndpoint: AsyncClientEndpoint? = null
+    private val inFlight = ArrayDeque<Request>()
+    private val connectionCallbackHandler: FutureCallback<AsyncClientEndpoint>
+    private var total = 0
+    private var burst = 0
+
+    init {
+        connectionCallbackHandler = ConnectionCallback(this)
+        createCon()
+    }
+
+    fun createCon() {
+        if (inFlight.isEmpty() && requestQueue.isEmpty() && successfulRequests.get() != 0) {
+            conclude()
+            return
+        }
+
+        // does this establish a new connection, or just a new channel?
+        requester.connect(target, Timeout.ofSeconds(5), null, connectionCallbackHandler)
+        //clientEndpoint = future.get();
+    }
+
+    fun connected(clientEndpoint: AsyncClientEndpoint) {
+        this.clientEndpoint = clientEndpoint
+        total = 0
+        triggerRequests()
+    }
+
+    private fun triggerRequests() {
+        if (inFlight.isEmpty() && requestQueue.isEmpty() && successfulRequests.get() != 0) {
+            conclude()
+            return
+        }
+
+        if (total >= requestsPerConnection) {
+            createCon()
+            return
+        }
+
+        if (inFlight.isEmpty()) {
+            burst = 0
+        }
+
+        while(burst < readFreq && total < requestsPerConnection) {
+            val target = requestQueue.poll() ?: break
+            request(target)
+            burst++
+            total++
+        }
+        //println("queued "+burst+ " requests")
+    }
+
+    private fun request(req: Request): Future<*> {
+
+        val requestProducer = BasicRequestProducer(req.request, req.dataProducer)
+
+        //val requestProducer = BasicRequestProducer("POST", URI("https://hackxor.net/static/cow"), null) // proves the dataProducer is the problem
+
+        //val requestProducer = BasicRequestProducer("GET", HttpHost("research1.hackxor.net", 443, "http2"), "/static/cow")
+        val consumer =  BasicResponseConsumer(StringAsyncEntityConsumer()) // StringAsyncEntityConsumer vs BasicAsyncEntityConsumer
+
+        val future = clientEndpoint!!.execute(
+                requestProducer,
+                consumer,
+                this)
+
+        inFlight.add(req)
+        return future
+    }
+
+    private fun connectionDropped() {
+        println("Connection dropped. Resetting")
+        if (!inFlight.isEmpty()) {
+            println("Re-queuing dropped requests")
+            requestQueue.addAll(inFlight)
+            inFlight.clear()
+        }
+        clientEndpoint!!.releaseAndDiscard();
+        createCon()
+    }
+
+
+    override fun completed(message: Message<HttpResponse, String>) {
+        successfulRequests.getAndIncrement()
+        val request = inFlight.pop()
+        if (callback != null) {
+            // todo convert these arguments into correct strings
+
+            callback.invoke(request.base, responseToString(message.head, message.body))
+        }
+
+        if (inFlight.isEmpty()) {
+            triggerRequests()
+        }
+    }
+
+    override fun failed(ex: Exception) {
+        println("Failed!: " + inFlight.peek() + "->" + ex)
+        connectionDropped()
+    }
+
+    override fun cancelled() {
+        System.out.println("Cancelled! " +inFlight.peek());
+        conclude()
+    }
+
+    private fun conclude() {
+        clientEndpoint?.releaseAndDiscard()
+        latch.countDown()
+        //print("Closing thread")
+    }
+
+
+    @Throws(IOException::class)
+    fun responseToString(resp: HttpResponse, body: String): String {
+        val output = StringBuilder()
+
+
+        output.append(resp.version)
+        output.append(" ")
+        output.append(resp.code)
+        output.append(" ")
+        output.append(resp.reasonPhrase)
+        output.append("\r\n")
+
+        val headers = resp.headerIterator()
+        while (headers.hasNext()) {
+            val header = headers.next()
+            output.append(header.name)
+            output.append(": ")
+            output.append(header.value)
+            output.append("\r\n")
+        }
+        output.append("\r\n")
+
+        output.append(body)
+
+        return output.toString()
+    }
+
+}
+
+internal class ConnectionCallback(private val con: Connection) : FutureCallback<AsyncClientEndpoint> {
+
+    override fun completed(endpoint: AsyncClientEndpoint) {
+        //System.out.println("Connection established");
+        con.connected(endpoint)
+    }
+
+    override fun failed(e: Exception) {
+        println("Attempt to establish new connection failed. Retrying.")
+        con.createCon()
+    }
+
+    override fun cancelled() {
+
+    }
+}
