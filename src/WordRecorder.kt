52b234c0d584812706663321dcea5a7dd61a2785
James Kettle <albinowax@gmail.com>
2018-05-11 11:36:04 +0100

    Add WordRecorder

---
+++ src/WordRecorder.kt
@@ -0,0 +1,24 @@
+package burp
+
+import java.util.*
+import java.util.concurrent.ConcurrentHashMap
+
+class WordRecorder: IScannerCheck {
+
+    val savedWords = ConcurrentHashMap.newKeySet<String>()
+    val wordRegex = "[^a-zA-Z0-9]".toRegex()
+
+    override fun doActiveScan(p0: IHttpRequestResponse?, p1: IScannerInsertionPoint?): MutableList<IScanIssue> {
+        return ArrayList()
+    }
+
+    override fun consolidateDuplicateIssues(p0: IScanIssue?, p1: IScanIssue?): Int {
+        return 0
+    }
+
+    override fun doPassiveScan(baseRequestResponse: IHttpRequestResponse?): MutableList<IScanIssue> {
+        savedWords.addAll(BurpExtender.callbacks.helpers.bytesToString(baseRequestResponse?.request).split(wordRegex))
+        savedWords.addAll(BurpExtender.callbacks.helpers.bytesToString(baseRequestResponse?.response).split(wordRegex))
+        return ArrayList()
+    }
+}
