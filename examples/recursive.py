20de9a4fef01c11e44e23276ff885ff7bbae022e
James Kettle <albinowax@gmail.com>
2019-01-18 10:54:39 +0000

    Add misc & recursive example scripts

---
+++ examples/recursive.py
@@ -0,0 +1,16 @@
+def queueRequests(target, wordlists):
+    engine = RequestEngine(endpoint=target.endpoint,
+                           concurrentConnections=5,
+                           requestsPerConnection=100,
+                           pipeline=False
+                           )
+    engine.start()
+    for word in open('/usr/share/dict/words'):
+        engine.queue(target.req, word.rstrip())
+
+
+def handleResponse(req, interesting):
+    if '404 Not Found' not in req.response:
+        table.add(req)
+        for word in open('/usr/share/dict/words'):
+            req.engine.queue(req.template, req.word+'/'+word.rstrip())
