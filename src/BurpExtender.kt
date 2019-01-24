0178098563a6a3d4fdc0a4a4d1bd97ebfec8bb5d
James Kettle <albinowax@gmail.com>
2019-01-24 13:17:26 +0000

    Bump version to 1.0.2

--- src/BurpExtender.kt
+++ src/BurpExtender.kt
@@ -2,7 +2,7 @@ package burp
 
 class BurpExtender(): IBurpExtender, IExtensionStateListener {
 
-    val version = "1.0.1"
+    val version = "1.0.2"
 
     override fun extensionUnloaded() {
         Utils.unloaded = true
