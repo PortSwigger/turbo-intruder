f8602d19476165ca73277d655d3bd2af1c31068c
James Kettle <albinowax@gmail.com>
2019-01-31 15:06:53 +0000

    Improve multi-host script

--- resources/examples/multiHost.py
+++ resources/examples/multiHost.py
@@ -1,7 +1,7 @@
 # Please note the stats panel won't reflect reality on multi-host attacks, and this example script isn't optimised for scale
 def queueRequests(target, wordlists):
-    base_req = '''GET / HTTP/1.1
-Host: %s.hostname
+    req = '''GET / HTTP/1.1
+Host: %s
 Connection: keep-alive
 
 '''
@@ -9,13 +9,11 @@ Connection: keep-alive
         engine = RequestEngine(endpoint='https://'+domain+':443')
         engine.start()
 
-        req = base_req.replace('hostname', domain)
-
         for i in range(3, 8):
-            engine.queue(req, randstr(i), learn=1)
+            engine.queue(req, randstr(i)+'.'+domain, learn=1)
 
         for word in open('/usr/share/dict/words'):
-            engine.queue(req, word.rstrip())
+            engine.queue(req, word.rstrip()+'.'+domain)
 
 
 def handleResponse(req, interesting):
