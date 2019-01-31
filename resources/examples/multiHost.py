97702326f495438ac9a264f74840b1743fbfd056
James Kettle <albinowax@gmail.com>
2019-01-31 15:20:51 +0000

    Make multi-host attack scale better

--- resources/examples/multiHost.py
+++ resources/examples/multiHost.py
@@ -1,19 +1,25 @@
-# Please note the stats panel won't reflect reality on multi-host attacks, and this example script isn't optimised for scale
+# Please note the stats panel won't reflect reality on multi-host attacks
 def queueRequests(target, wordlists):
     req = '''GET / HTTP/1.1
 Host: %s
 Connection: keep-alive
 
 '''
-    for domain in open('your domain list here'):
+    engines = {}
+    for domain in open('/tmp/domains'):
+        domain = domain.rstrip()
         engine = RequestEngine(endpoint='https://'+domain+':443')
         engine.start()
+        engines[domain] = engine
 
-        for i in range(3, 8):
+    for i in range(3, 8):
+        for (domain, engine) in engines.items():
             engine.queue(req, randstr(i)+'.'+domain, learn=1)
 
-        for word in open('/usr/share/dict/words'):
-            engine.queue(req, word.rstrip()+'.'+domain)
+    for word in open('/tmp/words'):
+        word = word.rstrip()
+        for (domain, engine) in engines.items():
+            engine.queue(req, word+'.'+domain)
 
 
 def handleResponse(req, interesting):
