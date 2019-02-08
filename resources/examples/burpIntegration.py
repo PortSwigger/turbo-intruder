69bb0ff38c9bb12f91d0f18094d66f29b4e33b85
James Kettle <albinowax@gmail.com>
2019-02-08 11:29:04 +0000

    Add burpIntegration example

--- resources/examples/burpIntegration.py
+++ resources/examples/burpIntegration.py
@@ -6,11 +6,15 @@ def queueRequests(target, wordlists):
                            )
     engine.start()
 
+    for i in range(3, 8):
+        engine.queue(target.req, randstr(i), learn=1)
+
     for word in open('/usr/share/dict/words'):
         engine.queue(target.req, word.rstrip())
 
-
 def handleResponse(req, interesting):
-    # currently available attributes are req.status, req.wordcount, req.length and req.response
-    if req.status != 404:
+    if interesting:
         table.add(req)
+        callbacks.addToSiteMap(req.getBurpRequest())
+        # You can also trigger scans, report issues, send to spider, etc:
+        # https://portswigger.net/burp/extender/api/burp/IBurpExtenderCallbacks.html
