8535b2e2edee5f99a64526df3fc1e8466bcb04b6
James Kettle <albinowax@gmail.com>
2019-09-03 09:48:37 +0100

    Add link to debug.py

--- src/fast-http.kt
+++ src/fast-http.kt
@@ -149,6 +149,7 @@ fun evalJython(code: String, baseRequest: String, endpoint: String, baseInput: S
             }
             handler.overrideStatus("User Python error, check extender for full details: "+message)
             Utils.out("There was an error executing your Python script. This is probably due to a flaw in your script, rather than a bug in Turbo Intruder :)")
+            Utils.out("If you think it is a Turbo Intruder issue, try out this script: https://raw.githubusercontent.com/PortSwigger/turbo-intruder/master/resources/examples/debug.py")
             Utils.out("For your convenience, here's the full stack trace:")
             Utils.out(stackTrace.toString())
         }
