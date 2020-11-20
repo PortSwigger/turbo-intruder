3bf481134c6cb45d8f211eaaf287f31d345f42df
James <james.kettle@portswigger.net>
2020-11-20 17:13:36 +0000

    Bump version

--- src/BurpExtender.kt
+++ src/BurpExtender.kt
@@ -3,7 +3,7 @@ package burp
 class BurpExtender(): IBurpExtender, IExtensionStateListener {
 
     companion object {
-        const val version = "1.1.2"
+        const val version = "1.1.3"
     }
 
     override fun extensionUnloaded() {
