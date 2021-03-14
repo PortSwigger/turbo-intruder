0d31e4e9a33c6b298e9deab29f8ebfea22915c49
Giriraj Singh <54558097+rajsng3737@users.noreply.github.com>
2021-03-14 18:21:49 +0530

    PitchFork Script for Turbo Intruder.
    PitchFork Script for Turbo Intruder. Executing till the end of the smaller payload file, if there are uneven number of payloads.

---
+++ resources/examples/pitchFork.py
@@ -0,0 +1,20 @@
+def queueRequests(target, wordlists):
+    engine = RequestEngine(endpoint=target.endpoint,
+                           concurrentConnections=5,
+                           requestsPerConnection=100,
+                           pipeline=False
+                           )
+    payload1 = []
+    payload2 = []
+    for eachWord in open('/usr/share/dict/numbers.txt'):
+        payload1.append(eachWord)
+    for eachWord in open('/usr/share/dict/posspass.txt'):
+        payload2.append(eachWord)
+    for i in range(0,len(payload2) if(len(payload2)<=len(payload1)) else len(paylaod1)): # execute till the length of smaller payload.
+        engine.queue(target.req, [payload1[i], payload2[i]])
+
+
+def handleResponse(req, interesting):
+    # currently available attributes are req.status, req.wordcount, req.length and req.response
+    if req.status != 404:
+        table.add(req)
