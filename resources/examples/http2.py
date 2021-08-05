8f9627fcd01859d9973a15393c947a8d69d87652
James <james.kettle@portswigger.net>
2021-08-05 14:59:47 +0100

    Add more explanation to http2 example

--- resources/examples/http2.py
+++ resources/examples/http2.py
@@ -5,6 +5,13 @@ def queueRequests(target, wordlists):
                            engine=Engine.HTTP2 # To use Burp's HTTP/2 stack instead, use Engine.BURP2
                            )
 
+     # When using either HTTP/2 engine, the following rewrites are performed:
+     # ^ -> \r
+     # ~ -> \n
+     # ` -> :
+     # You can override pseudo-headers by specifying them like normal headers
+     # eg ':path: /robots.txt'
+
     for word in open('/usr/share/dict/words'):
         engine.queue(target.req, word.rstrip())
 
