6aedfeac23144c26a687d5376616699274a4937e
James <james.kettle@portswigger.net>
2021-08-23 14:35:12 +0100

    Bump version to 1.22

--- src/BurpExtender.kt
+++ src/BurpExtender.kt
@@ -3,7 +3,7 @@ package burp
 class BurpExtender(): IBurpExtender, IExtensionStateListener {
 
     companion object {
-        const val version = "1.21"
+        const val version = "1.22"
     }
 
     override fun extensionUnloaded() {
