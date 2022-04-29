c2c48f49d3aa4c89afed017843d4eaf937fb5249
Bryan Eduria <eduria.bryan.96@gmail.com>
2022-04-29 09:29:45 +0800

    strip payload space on pitchfork wordlist

--- resources/examples/pitchFork.py
+++ resources/examples/pitchFork.py
@@ -7,9 +7,9 @@ def queueRequests(target, wordlists):
     payload1 = []
     payload2 = []
     for eachWord in open('/usr/share/dict/numbers.txt'):
-        payload1.append(eachWord)
+        payload1.append(eachWord.strip())
     for eachWord in open('/usr/share/dict/posspass.txt'):
-        payload2.append(eachWord)
+        payload2.append(eachWord.strip())
     for i in range(0,len(payload2) if(len(payload2)<=len(payload1)) else len(payload1)):
         # execute till the length of smaller payload.
         engine.queue(target.req, [payload1[i], payload2[i]])
