368e719f6afaf46fd0b8592d956288154bac6094
James Kettle <albinowax@gmail.com>
2023-08-25 10:35:58 +0100

    Bump version to 1.41

--- src/BurpExtender.kt
+++ src/BurpExtender.kt
@@ -7,7 +7,7 @@ import javax.swing.SwingUtilities
 class BurpExtender() : IBurpExtender, IExtensionStateListener, BurpExtension {
 
     companion object {
-        const val version = "1.40"
+        const val version = "1.41"
     }
 
     override fun extensionUnloaded() {
