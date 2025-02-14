c602a0dafe4a9f2e930a0efaa291471db4180d75
James Kettle <james.kettle@portswigger.net>
2025-02-14 11:52:44 +0000

    bump version

--- src/BurpExtender.kt
+++ src/BurpExtender.kt
@@ -11,7 +11,7 @@ import javax.swing.SwingUtilities
 class BurpExtender() : IBurpExtender, IExtensionStateListener, BurpExtension {
 
     companion object {
-        const val version = "1.51"
+        const val version = "1.52"
     }
 
     override fun extensionUnloaded() {
