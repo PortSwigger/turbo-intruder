eb49652a925dd5e44f6e1e7c3414069fe932b4e4
James Kettle <james.kettle@portswigger.net>
2025-11-14 11:44:08 +0000

    Add support for hotkey / command palette

--- src/BurpExtender.kt
+++ src/BurpExtender.kt
@@ -2,9 +2,23 @@ package burp
 
 import burp.api.montoya.BurpExtension
 import burp.api.montoya.MontoyaApi
+import burp.api.montoya.http.message.HttpHeader
+import burp.api.montoya.http.message.HttpRequestResponse
+import burp.api.montoya.ui.contextmenu.MessageEditorHttpRequestResponse
+import burp.api.montoya.ui.contextmenu.MessageEditorHttpRequestResponse.SelectionContext
+import burp.api.montoya.ui.hotkey.HotKey
+import burp.api.montoya.ui.hotkey.HotKeyContext
+import burp.api.montoya.ui.hotkey.HotKeyEvent
+import burp.api.montoya.ui.hotkey.HotKeyHandler
+import java.awt.datatransfer.Clipboard
+import java.awt.datatransfer.StringSelection
+import java.util.function.Consumer
+import java.util.stream.Collectors
+import javax.swing.JFrame
 import javax.swing.JMenuItem
 import javax.swing.SwingUtilities
-import javax.swing.JFrame
+import kotlin.jvm.optionals.getOrNull
+
 
 class BurpExtender() : IBurpExtender, IExtensionStateListener, BurpExtension {
 
@@ -35,6 +49,34 @@ class BurpExtender() : IBurpExtender, IExtensionStateListener, BurpExtension {
         Utils.montoyaApi = montoyaApi
         Utilities.montoyaApi = montoyaApi
         montoyaApi.userInterface().registerContextMenuItemsProvider(BulkMenu())
+        registerHotkey(montoyaApi)
+    }
+
+    fun registerHotkey(montoyaApi: MontoyaApi) {
+        try {
+            val hotKey: HotKey? = HotKey.hotKey("Send to Turbo Intruder", "Ctrl+Alt-T");
+            val handler = HotKeyHandler { event: HotKeyEvent? ->
+                event!!.messageEditorRequestResponse().ifPresent(Consumer { editor: MessageEditorHttpRequestResponse? ->
+                    val requestResponse = editor!!.requestResponse()
+                    val inputReq = Resp(requestResponse)
+                    val selectionOffsets = editor.selectionOffsets().getOrNull()
+                    var bounds = intArrayOf()
+                    if (selectionOffsets != null) {
+                        bounds =
+                            intArrayOf(selectionOffsets.startIndexInclusive(), selectionOffsets.endIndexExclusive())
+                    }
+                    TurboIntruderFrame(inputReq, bounds, null, null, null).actionPerformed(null)
+                })
+            }
+
+            montoyaApi.userInterface().registerHotKeyHandler(
+                HotKeyContext.HTTP_MESSAGE_EDITOR,
+                hotKey,
+                handler
+            );
+        } catch (e: NoSuchMethodError) {
+            // Utils.out("Please update Burp Suite to the latest available version")
+        }
 
         // Keep Montoya registrations minimal to avoid duplicating the existing top-level menu
     }
